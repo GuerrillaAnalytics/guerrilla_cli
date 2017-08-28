7c8456a908167528b0a97b045f0e048c53c146fa
Enda Ridge <guerrillaanalytics@gmail.com>
2017-08-28 21:25:27 +0100

    tidy up and simplify

--- guerrilla/commands.py
+++ guerrilla/commands.py
@@ -1,15 +1,10 @@
 import click
 
 
-@cli.group()
-@click.pass_context
-def uptimerobot(ctx):
-    pass
-
-@uptimerobot.command('add')
-@click.option('--alert', '-a', default=True)
-@click.argument('name')
-@click.argument('url')
-@click.pass_obj
-def uptimerobot_add(ctx, name, url, alert):
-    pass
+@click.command()
+@click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
+@click.argument('name', default='world', required=False)
+def main(name, as_cowboy):
+    """My Tool does one thing, and one thing well."""
+    greet = 'Howdy' if as_cowboy else 'Hello'
+    click.echo('{0}, {1}.'.format(greet, name))
