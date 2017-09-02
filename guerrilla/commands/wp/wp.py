db89f13624896e0f95747d693b5c74676f3932ae
Enda Ridge <guerrillaanalytics@gmail.com>
2017-09-02 06:36:51 +0100

    corrections

--- guerrilla/commands/wp/wp.py
+++ guerrilla/commands/wp/wp.py
@@ -6,46 +6,11 @@ import click
 def wp(ctx):
     pass
 
-#example of another subgroup
-@wp.group('zone')
-def wp_zone():
-    pass
 
-
-@wp_zone.command('add')
-@click.option('--jumpstart', '-j', default=True)
-@click.option('--organization', '-o', default='')
-@click.argument('url')
+@wp.command('add')
+@click.argument('id', '-i', default="wp")
+@click.argument('label', '-l', default='')
 @click.pass_obj
-# @__cf_error_handler
-def wp_zone_add(ctx, url, jumpstart, organization):
-    pass
 
-
-@wp.group('record')
-def wp_record():
-    pass
-
-
-@wp_record.command('add')
-@click.option('--ttl', '-t')
-@click.argument('domain')
-@click.argument('name')
-@click.argument('type')
-@click.argument('content')
-@click.pass_obj
-# @__cf_error_handler
-def wp_record_add(ctx, domain, name, type, content, ttl):
-    pass
-
-
-@wp_record.command('edit')
-@click.option('--ttl', '-t')
-@click.argument('domain')
-@click.argument('name')
-@click.argument('type')
-@click.argument('content')
-@click.pass_obj
-# @__cf_error_handler
-def wp_record_edit(ctx, domain):
+def wp_add(ctx, url, jumpstart, organization):
     pass
